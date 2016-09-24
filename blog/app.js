30ce4fb144337e0754462b9fbcb23d21de5d07da
Lyf <517209885@qq.com>
2016-09-25 00:19:41 +0800

    添加路由

--- blog/app.js
+++ blog/app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// 引入 router
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -30,8 +30,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+routes(app)
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
